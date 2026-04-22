fcc7bcc903f6099c3f939f491de6122ec4eacc98
mdutka5 <marcin.dutka.05@gmail.com>
2026-04-22 08:44:15 -0400

    initial commit

---
+++ src/main/java/com/github/mdutka5/simplifiedstockmarket/SimplifiedStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.mdutka5.simplifiedstockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedStockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplifiedStockMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mdutka5/simplifiedstockmarket/SimplifiedStockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mdutka5.simplifiedstockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedStockMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
