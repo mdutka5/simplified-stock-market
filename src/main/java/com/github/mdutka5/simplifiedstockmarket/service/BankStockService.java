4a078cb5176b6ac0f44ca6dd623aefa68b18fb10
mdutka5 <marcin.dutka.05@gmail.com>
2026-04-23 13:20:51 -0400

    Add BankStockService

--- src/main/java/com/github/mdutka5/simplifiedstockmarket/service/BankStockService.java
+++ src/main/java/com/github/mdutka5/simplifiedstockmarket/service/BankStockService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.github.mdutka5.simplifiedstockmarket.repository.BankStockRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +17,16 @@ public class BankStockService {
         return bankStockRepository.findAll();
     }
 
-    public void setBankState(Iterable<BankStock> bankStocks) {
+    public void setBankState(List<BankStock> bankStocks) {
+        bankStockRepository.deleteAll();
+        bankStockRepository.saveAll(bankStocks);
+    }
 
+    public Optional<BankStock> findByStockName(String stockName) {
+        return bankStockRepository.findById(stockName);
+    }
+
+    public void updateStock(BankStock bankStock) {
+        bankStockRepository.save(bankStock);
     }
 }
