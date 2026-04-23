cfc1232897fa03bf7d3b64af643799a2ba6e3899
mdutka5 <marcin.dutka.05@gmail.com>
2026-04-23 14:28:11 -0400

    Add custom repository query method

--- src/main/java/com/github/mdutka5/simplifiedstockmarket/repository/WalletStockRepository.java
+++ src/main/java/com/github/mdutka5/simplifiedstockmarket/repository/WalletStockRepository.java
@@ -3,5 +3,8 @@ package com.github.mdutka5.simplifiedstockmarket.repository;
 import com.github.mdutka5.simplifiedstockmarket.model.WalletStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletStockRepository extends JpaRepository<WalletStock, Long> {
+    List<WalletStock> findAllByWalletId(String walletId);
 }
