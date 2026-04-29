5cc6a316980365260a94b03512092111e55ca23e
mdutka5 <marcin.dutka.05@gmail.com>
2026-04-29 03:50:38 -0400

    feat: implement chaos controller

--- src/main/java/com/github/mdutka5/simplifiedstockmarket/controller/ChaosController.java
+++ src/main/java/com/github/mdutka5/simplifiedstockmarket/controller/ChaosController.java
@@ -1,4 +1,20 @@
 package com.github.mdutka5.simplifiedstockmarket.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stocks")
+@RequiredArgsConstructor
 public class ChaosController {
+    private final ApplicationContext applicationContext;
+
+    @PostMapping("/chaos")
+    public void chaos() {
+        System.exit(SpringApplication.exit(applicationContext));
+    }
 }
