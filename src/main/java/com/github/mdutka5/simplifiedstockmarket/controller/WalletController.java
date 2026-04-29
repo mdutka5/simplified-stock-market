129e34782db5d7a013a240f0d93d87ff7023fd91
mdutka5 <marcin.dutka.05@gmail.com>
2026-04-29 03:50:08 -0400

    feat: implement wallet controller

--- src/main/java/com/github/mdutka5/simplifiedstockmarket/controller/WalletController.java
+++ src/main/java/com/github/mdutka5/simplifiedstockmarket/controller/WalletController.java
@@ -1,4 +1,36 @@
 package com.github.mdutka5.simplifiedstockmarket.controller;
 
+import com.github.mdutka5.simplifiedstockmarket.dto.request.TradeRequest;
+import com.github.mdutka5.simplifiedstockmarket.dto.response.WalletResponse;
+import com.github.mdutka5.simplifiedstockmarket.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/wallets")
+@RequiredArgsConstructor
 public class WalletController {
+    private final WalletService walletService;
+
+    @GetMapping("/{walletId}")
+    public ResponseEntity<WalletResponse> getWallet(@PathVariable String walletId) {
+        return ResponseEntity.ok(walletService.getWallet(walletId));
+    }
+
+    @GetMapping("/{walletId}/stocks/{stockName}")
+    public ResponseEntity<Integer> getWalletStock(
+            @PathVariable String walletId,
+            @PathVariable String stockName) {
+        return ResponseEntity.ok(walletService.getWalletStock(walletId, stockName));
+    }
+
+    @PostMapping("/{walletId}/stocks/{stockName}")
+    public ResponseEntity<Void> trade(
+            @PathVariable String walletId,
+            @PathVariable String stockName,
+            @RequestBody TradeRequest request) {
+        walletService.trade(walletId, stockName, request.getType());
+        return ResponseEntity.ok().build();
+    }
 }
